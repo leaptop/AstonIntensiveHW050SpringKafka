41ac61ea4c4a4949e47d7ce9e85dd15e36632b62
Stepan csl <cslpnj@gmail.com>
2026-03-12 20:35:31 +0300

    ДЗ 7 Переход на spring cloud Добавить к существующей системе паттерны: gateway api, service discovery, circuit breaker, external configuration - реализации данных паттернов можно найти в модулях spring cloud.

--- src/main/java/com/alekseev/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/alekseev/notificationservice/NotificationServiceApplication.java
@@ -2,8 +2,16 @@ package com.alekseev.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Главный класс notification-service.
+ *
+ * @EnableDiscoveryClient включает регистрацию в Eureka.
+ * Конфигурация загружается из Config Server.
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
